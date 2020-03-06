38655f624b3d9b82e47cdd0c54631eae66dab1e3
pdcmsbotball <61713448+pdcmsbotball@users.noreply.github.com>
2020-03-06 13:08:01 -0800

    added code for removing coupler

---
+++ main (1).c
@@ -0,0 +1,26 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    printf("Starting...\n");
+    shut_down_in(118);
+    
+    //turn right
+    mav(2,-1500);
+    mav(3,1500);
+    msleep(780);
+    
+    //forward
+    mav(2,1500);
+    mav(3,1500);
+    msleep(2600);
+    
+    //turn left
+    mav(2,1500);
+    mav(3,-1500);
+    msleep(1400);
+    
+    
+     
+    return 0;
+}
